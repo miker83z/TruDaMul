95713be8c8a44a595a068d4bdb17a976be92c29e
miker <mikerz83@gmail.com>
2021-08-27 19:01:29 +0200

    main smart contract complete

--- contracts/TruDaMul.sol
+++ contracts/TruDaMul.sol
@@ -19,81 +19,153 @@ contract TruDaMul {
     // Amount of tokens reserved for open tender offers
     uint256 public _openTendersOffers;
 
+    // Grace period (in ms) for tenders before authorized address can be modified
+    uint256 public _tenderGracePeriod;
+
     // Tenders data structure
+    mapping(bytes => uint256) tendersByexID;
     mapping(uint256 => Tender) tenders;
     uint256 tendersNum;
     struct Tender {
-        bool open;
-        bytes eID;
+        uint256 gracePeriodEnd;
+        bool fulfilled;
+        bytes exID;
         uint256 senderOffer;
         uint256 idx;
         bytes pURI;
-        uint256 muleRequest;
+        address authorized;
     }
     event TenderAnnouncement(
-        bytes indexed eID,
+        uint256 gracePeriodEnd,
+        bytes indexed exID,
         uint256 senderOffer,
         uint256 idx,
         bytes indexed pURI,
-        uint256 muleRequest
+        address authorized
     );
 
     // Mule Payments data structure
     mapping(address => MulePayments) mulePayments;
     struct MulePayments {
+        mapping(bytes => uint256) paymentsByexID;
         mapping(uint256 => Payment) payments;
         uint256 paymentsNum;
     }
     struct Payment {
-        bytes eID;
+        bytes exID;
         bool paid;
     }
 
-    event Debug(bytes32 indexed deb);
+    event Debug(bytes indexed deb);
 
     constructor(address tokenAddress, address sender) {
         require(tokenAddress != address(0), "TruDaMul: Invalid token address");
         _token = IERC20(tokenAddress);
         _sender = sender;
+        _tenderGracePeriod = 600000;
     }
 
-    function muleTenderAnnouncement(
-        bytes memory eID,
+    function submitTender(
+        bytes memory exID,
         uint256 senderOffer,
         uint256 idx,
         bytes memory pURI,
-        bytes memory senderSignature,
-        uint256 muleRequest
+        bytes memory senderTenderSignature,
+        address mule,
+        bytes memory senderMuleSignature,
+        address authorized
     ) public {
-        require(
-            _token.balanceOf(address(this)) - _openTendersOffers >= senderOffer,
-            "TruDaMul: Insufficient balance"
-        );
+        // Tender signature check
         address extractedSender = extractTenderSignature(
-            eID,
+            exID,
             senderOffer,
             idx,
             pURI,
+            senderTenderSignature
+        );
+        require(_sender == extractedSender, "TruDaMul: Invalid signature");
+
+        // Mule address signature check
+        address extractedSender2 = extractMuleSignature(
+            mule,
+            senderMuleSignature
+        );
+        require(_sender == extractedSender2, "TruDaMul: Invalid signature");
+
+        // Allow mule state channel payment
+        _allowMulePayment(mule, exID);
+
+        // Check balance
+        if (
+            _token.balanceOf(address(this)) - _openTendersOffers < senderOffer
+        ) {
+            return;
+        } else {
+            tendersByexID[exID] = ++tendersNum;
+            Tender storage t = tenders[tendersNum];
+            t.gracePeriodEnd = block.timestamp + _tenderGracePeriod;
+            t.exID = exID;
+            t.senderOffer = senderOffer;
+            t.idx = idx;
+            t.pURI = pURI;
+            t.authorized = authorized;
+
+            _openTendersOffers += senderOffer;
+
+            emit TenderAnnouncement(
+                t.gracePeriodEnd,
+                exID,
+                senderOffer,
+                idx,
+                pURI,
+                authorized
+            );
+        }
+    }
+
+    function submitPayment(
+        bytes memory exID,
+        bytes memory mPayID,
+        bytes memory pPayID,
+        bytes memory senderSignature,
+        address mule,
+        bytes memory senderMuleSignature
+    ) public {
+        uint256 exIDIndex = tendersByexID[exID];
+        require(exIDIndex > 0, "TruDaMul: Invalid exID");
+
+        // Mule address signature check
+        address extractedSender2 = extractMuleSignature(
+            mule,
+            senderMuleSignature
+        );
+        require(_sender == extractedSender2, "TruDaMul: Invalid signature");
+
+        // Payments signature check
+        address extractedSender = extractPaymentsSignature(
+            exID,
+            mPayID,
+            pPayID,
             senderSignature
         );
+        if (_sender == extractedSender) {
+            // Pay proxy
+            Tender storage t = tenders[exIDIndex];
+            _token.transfer(t.authorized, t.senderOffer);
+            _openTendersOffers -= t.senderOffer;
+            t.fulfilled = true;
+        } else {
+            // Mule payment signature check
+            address extractedSender3 = extractMulePaymentSignature(
+                exID,
+                mPayID,
+                senderSignature
+            );
+            require(_sender == extractedSender3, "TruDaMul: Invalid signature");
+        }
 
-        require(_sender == extractedSender, "TruDaMul: Invalid signature");
-        //TODO: check valid mule address (e.g. sender signature)
-
-        Tender storage t = tenders[tendersNum++];
-        t.open = true;
-        t.eID = eID;
-        t.senderOffer = senderOffer;
-        t.idx = idx;
-        t.pURI = pURI;
-        t.muleRequest = muleRequest;
-
-        MulePayments storage p = mulePayments[msg.sender];
-        p.payments[p.paymentsNum++].eID = eID;
-
-        _openTendersOffers += senderOffer;
-
-        emit TenderAnnouncement(eID, senderOffer, idx, pURI, muleRequest);
+        // Allow mule state channel payment
+        _allowMulePayment(mule, exID);
     }
 
     /**
@@ -101,46 +173,119 @@ contract TruDaMul {
      * @return Address of the tender signer.
      */
     function extractTenderSignature(
-        bytes memory eID,
+        bytes memory exID,
         uint256 senderOffer,
         uint256 idx,
         bytes memory pURI,
         bytes memory senderSignature
     ) public pure returns (address) {
         // Compute message hash
-        bytes32 hash = keccak256(abi.encodePacked(eID, senderOffer, idx, pURI));
+        bytes32 hash = keccak256(
+            abi.encodePacked(exID, senderOffer, idx, pURI)
+        );
 
         // Derive address from signature
         return
             ECDSA.recover(ECDSA.toEthSignedMessageHash(hash), senderSignature);
     }
 
-    function checkMulePayment(address mule, bytes memory eID)
+    /**
+     * @notice Returns the address extracted from the mule address signature.
+     * @return Address of the mule address signer.
+     */
+    function extractMuleSignature(address mule, bytes memory senderSignature)
+        public
+        pure
+        returns (address)
+    {
+        // Compute message hash
+        bytes32 hash = keccak256(abi.encodePacked(mule));
+
+        // Derive address from signature
+        return
+            ECDSA.recover(ECDSA.toEthSignedMessageHash(hash), senderSignature);
+    }
+
+    /**
+     * @notice Returns the address extracted from payments signature.
+     * @return Address of the payments signer.
+     */
+    function extractPaymentsSignature(
+        bytes memory exID,
+        bytes memory mPayID,
+        bytes memory pPayID,
+        bytes memory senderSignature
+    ) public pure returns (address) {
+        // Compute message hash
+        bytes32 hash = keccak256(abi.encodePacked(exID, mPayID, pPayID));
+
+        // Derive address from signature
+        return
+            ECDSA.recover(ECDSA.toEthSignedMessageHash(hash), senderSignature);
+    }
+
+    /**
+     * @notice Returns the address extracted from payments signature.
+     * @return Address of the payments signer.
+     */
+    function extractMulePaymentSignature(
+        bytes memory exID,
+        bytes memory mPayID,
+        bytes memory senderSignature
+    ) public pure returns (address) {
+        // Compute message hash
+        bytes32 hash = keccak256(abi.encodePacked(exID, mPayID));
+
+        // Derive address from signature
+        return
+            ECDSA.recover(ECDSA.toEthSignedMessageHash(hash), senderSignature);
+    }
+
+    function _allowMulePayment(address mule, bytes memory exID) private {
+        MulePayments storage p = mulePayments[mule];
+        if (p.paymentsByexID[exID] == 0) {
+            p.paymentsByexID[exID] = ++p.paymentsNum;
+            p.payments[p.paymentsNum].exID = exID;
+        }
+        p.payments[p.paymentsByexID[exID]].paid = true;
+    }
+
+    function checkMulePayment(address mule, bytes memory exID)
         public
         view
         returns (bool)
     {
         MulePayments storage p = mulePayments[mule];
-        bool flag = false;
-        for (uint256 i = 0; i < p.paymentsNum && !flag; i++) {
-            if (p.payments[i].paid == false) return false;
-            if (
-                keccak256(abi.encodePacked(p.payments[i].eID)) ==
-                keccak256(abi.encodePacked(eID))
-            ) flag = true;
+        if (p.paymentsByexID[exID] == 0) {
+            return false;
+        } else {
+            bool flag = false;
+            for (uint256 i = 1; i <= p.paymentsNum && !flag; i++) {
+                if (p.payments[i].paid == false) return false;
+                if (
+                    keccak256(abi.encodePacked(p.payments[i].exID)) ==
+                    keccak256(abi.encodePacked(exID))
+                ) flag = true;
+            }
+            return flag;
         }
-        return flag;
     }
 
-    function setPaidTemp(address mule, bytes memory eID) public {
-        MulePayments storage p = mulePayments[mule];
-        for (uint256 i = 0; i < p.paymentsNum; i++) {
-            if (
-                keccak256(abi.encodePacked(p.payments[i].eID)) ==
-                keccak256(abi.encodePacked(eID))
-            ) {
-                p.payments[i].paid = true;
-                return;
+    /// Authorization Service method
+    function checkPermissions(address user, bytes memory exID)
+        public
+        view
+        returns (bool)
+    {
+        uint256 tId = tendersByexID[exID];
+        if (tId == 0) {
+            return false;
+        } else {
+            Tender storage t = tenders[tId];
+            if (user == t.authorized) {
+                return true;
+            } else {
+                return false;
             }
         }
     }
--- test/test.js
+++ test/test.js
@@ -6,9 +6,12 @@ contract('PaymentsChannel', async (accounts) => {
   const owner = accounts[0];
   const sender = accounts[1];
   const mule = accounts[2];
+  const proxy = accounts[3];
+  const mule2 = accounts[4];
 
   let blockNumber;
-  const eID = web3.utils.asciiToHex('0001');
+  let blockNumber2;
+  const exID = web3.utils.asciiToHex('0001');
   const pURI = web3.utils.asciiToHex('ImmutableURI');
 
   it('releases tokens to the accounts', async () => {
@@ -16,13 +19,13 @@ contract('PaymentsChannel', async (accounts) => {
     let balance = await token.balanceOf.call(sender);
     assert.equal(balance.toString(), '0');
 
-    await token.transfer(sender, 10000, {
+    await token.transfer(sender, 20000, {
       from: owner,
     });
     balance = await token.balanceOf.call(sender);
     assert.equal(
       balance.toString(),
-      '10000',
+      '20000',
       'Amount was not correctly minted'
     );
 
@@ -37,27 +40,31 @@ contract('PaymentsChannel', async (accounts) => {
     );
   });
 
-  it('opens a channel between sender and mule', async () => {
+  it('opens a channel between sender and the two mules', async () => {
     const token = await MuleToken.deployed();
     const channel = await StateChannel.deployed();
 
-    await token.approve(StateChannel.address, 5000, {
+    await token.approve(StateChannel.address, 10000, {
       from: sender,
     });
     const result = await channel.createChannel(mule, 5000, {
       from: sender,
     });
     blockNumber = result.receipt.blockNumber;
+    const result2 = await channel.createChannel(mule2, 5000, {
+      from: sender,
+    });
+    blockNumber2 = result2.receipt.blockNumber;
   });
 
-  it('announce a new tender', async () => {
+  it('mule sumbits a new tender', async () => {
     const token = await MuleToken.deployed();
     const trudamul = await TruDaMul.deployed();
 
     const tenderHash = web3.utils.soliditySha3(
       {
         type: 'bytes',
-        value: eID,
+        value: exID,
       },
       {
         type: 'uint256',
@@ -72,29 +79,97 @@ contract('PaymentsChannel', async (accounts) => {
         value: pURI,
       }
     );
-    let senderSignature = await web3.eth.sign(tenderHash, sender);
-    senderSignature =
-      senderSignature.substr(0, 130) +
-      (senderSignature.substr(130) == '00' ? '1b' : '1c');
+    let senderTenderSignature = await web3.eth.sign(tenderHash, sender);
+    senderTenderSignature =
+      senderTenderSignature.substr(0, 130) +
+      (senderTenderSignature.substr(130) == '00' ? '1b' : '1c');
 
-    await token.transfer(TruDaMul.address, 200, {
+    const muleHash = web3.utils.soliditySha3({
+      type: 'address',
+      value: mule,
+    });
+    let senderMuleSignature = await web3.eth.sign(muleHash, sender);
+    senderMuleSignature =
+      senderMuleSignature.substr(0, 130) +
+      (senderMuleSignature.substr(130) == '00' ? '1b' : '1c');
+
+    await token.transfer(TruDaMul.address, 10, {
       from: sender,
     });
 
-    const res = await trudamul.muleTenderAnnouncement(
-      eID,
+    const res = await trudamul.submitTender(
+      exID,
       100,
       1,
       pURI,
-      senderSignature,
-      10,
+      senderTenderSignature,
+      mule,
+      senderMuleSignature,
+      mule,
       {
         from: mule,
       }
     );
   });
 
-  it('closes a channel', async () => {
+  it('proxy sumbits a new tender', async () => {
+    const token = await MuleToken.deployed();
+    const trudamul = await TruDaMul.deployed();
+
+    const tenderHash = web3.utils.soliditySha3(
+      {
+        type: 'bytes',
+        value: exID,
+      },
+      {
+        type: 'uint256',
+        value: 100,
+      },
+      {
+        type: 'uint256',
+        value: 1,
+      },
+      {
+        type: 'bytes',
+        value: pURI,
+      }
+    );
+    let senderTenderSignature = await web3.eth.sign(tenderHash, sender);
+    senderTenderSignature =
+      senderTenderSignature.substr(0, 130) +
+      (senderTenderSignature.substr(130) == '00' ? '1b' : '1c');
+
+    const muleHash = web3.utils.soliditySha3({
+      type: 'address',
+      value: mule,
+    });
+    let senderMuleSignature = await web3.eth.sign(muleHash, sender);
+    senderMuleSignature =
+      senderMuleSignature.substr(0, 130) +
+      (senderMuleSignature.substr(130) == '00' ? '1b' : '1c');
+
+    await token.transfer(TruDaMul.address, 200, {
+      from: sender,
+    });
+
+    const res = await trudamul.submitTender(
+      exID,
+      100,
+      1,
+      pURI,
+      senderTenderSignature,
+      mule,
+      senderMuleSignature,
+      proxy,
+      {
+        from: proxy,
+      }
+    );
+
+    const res2 = await trudamul.checkPermissions(proxy, exID, { from: proxy });
+  });
+
+  it('mule closes a channel', async () => {
     const channel = await StateChannel.deployed();
     const trudamul = await TruDaMul.deployed();
     const balance = 1300;
@@ -145,18 +220,128 @@ contract('PaymentsChannel', async (accounts) => {
       senderSign.substr(0, 130) +
       (senderSign.substr(130) == '00' ? '1b' : '1c');
 
-    await trudamul.setPaidTemp(mule, eID);
-
     await channel.closeChannel(
       mule,
       blockNumber,
       balance,
       senderSign,
       receiverSign,
-      eID,
+      exID,
       TruDaMul.address,
       {
         from: mule,
+      }
+    );
+  });
+
+  it('mule2 sumbits payments', async () => {
+    const token = await MuleToken.deployed();
+    const trudamul = await TruDaMul.deployed();
+    const mPayID = web3.utils.asciiToHex('0002');
+    const pPayID = web3.utils.asciiToHex('0003');
+
+    const paymentsHash = web3.utils.soliditySha3(
+      {
+        type: 'bytes',
+        value: exID,
+      },
+      {
+        type: 'bytes',
+        value: mPayID,
+      },
+      {
+        type: 'bytes',
+        value: pPayID,
+      }
+    );
+    let senderPaymentsSignature = await web3.eth.sign(paymentsHash, sender);
+    senderPaymentsSignature =
+      senderPaymentsSignature.substr(0, 130) +
+      (senderPaymentsSignature.substr(130) == '00' ? '1b' : '1c');
+
+    const muleHash = web3.utils.soliditySha3({
+      type: 'address',
+      value: mule2,
+    });
+    let senderMuleSignature = await web3.eth.sign(muleHash, sender);
+    senderMuleSignature =
+      senderMuleSignature.substr(0, 130) +
+      (senderMuleSignature.substr(130) == '00' ? '1b' : '1c');
+
+    const res = await trudamul.submitPayment(
+      exID,
+      mPayID,
+      pPayID,
+      senderPaymentsSignature,
+      mule2,
+      senderMuleSignature,
+      {
+        from: mule2,
+      }
+    );
+  });
+
+  it('mule2 closes a channel', async () => {
+    const channel = await StateChannel.deployed();
+    const trudamul = await TruDaMul.deployed();
+    const balance = 1300;
+
+    const receiverHash = web3.utils.soliditySha3(
+      {
+        type: 'address',
+        value: sender,
+      },
+      {
+        type: 'uint32',
+        value: blockNumber2,
+      },
+      {
+        type: 'uint192',
+        value: balance,
+      },
+      {
+        type: 'address',
+        value: StateChannel.address,
+      }
+    );
+    let receiverSign = await web3.eth.sign(receiverHash, mule2);
+    receiverSign =
+      receiverSign.substr(0, 130) +
+      (receiverSign.substr(130) == '00' ? '1b' : '1c');
+
+    const senderHash = web3.utils.soliditySha3(
+      {
+        type: 'address',
+        value: mule2,
+      },
+      {
+        type: 'uint32',
+        value: blockNumber2,
+      },
+      {
+        type: 'uint192',
+        value: balance,
+      },
+      {
+        type: 'address',
+        value: StateChannel.address,
+      }
+    );
+    let senderSign = await web3.eth.sign(senderHash, sender);
+    senderSign =
+      senderSign.substr(0, 130) +
+      (senderSign.substr(130) == '00' ? '1b' : '1c');
+
+    await channel.closeChannel(
+      mule2,
+      blockNumber2,
+      balance,
+      senderSign,
+      receiverSign,
+      exID,
+      TruDaMul.address,
+      {
+        from: mule2,
       }
     );
   });
